4ab4c6f1ff70656d9d0907a869bc8daf4561dc33
josiee-y <ysyjwq@gmail.com>
2023-07-31 10:59:25 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -9,9 +9,10 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = 500;
+  pWallpaper.grid_settings.cell_height = 350;
   pWallpaper.grid_settings.row_offset  = 50;
+
 }
 
 function wallpaper_background() {
@@ -23,33 +24,52 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   stroke(123)
   //fill(91, 193, 222);//light blue
   
-  var x = 110
+  //var x = 110
   
-  beginShape(TRIANGLES);
-  vertex(120,70);
-  vertex(100,90);
-  vertex(150,65);
-  endShape();
-
-  beginShape(TRIANGLES);
-  vertex(x,70);
-  vertex(x,87);
-  vertex(150,85);
-  endShape();
-
-  ellipse(x, x-20, x-30,x-30); //small circle 100, 80, 70,70
-  ellipse(x, x+45, x,x-10); //big circle 100,145, 100,90
-  ellipse(x+10, x-25, x-85,x-85); //eye 110,75,15,15
-
-  beginShape();
-  vertex(98,127);
-  vertex(76,149);
-  vertex(95,140);
-  //vertex(100,150);
-  //vertex(1,140);
-  vertex(117,150);
+  beginShape(); //rect behind
+  vertex(280,10); //TR
+  vertex(210,30); //TL
+  vertex(299,330); //BL
+  vertex(369,310); //BR
   endShape(CLOSE);
-  //ellipse(100, 330, 20, 80);
+  
+  beginShape(TRIANGLES); //tri left-top
+  vertex(63,80); //top point
+  vertex(65,105); //l
+  vertex(85,90); // r
+  endShape();
+
+  beginShape(TRIANGLES); //tri right-top
+  vertex(400,64); //top point
+  vertex(330,80); //left
+  vertex(370,130); //right
+  endShape();
+
+  beginShape(TRIANGLES); //tri left-bottom
+  vertex(150,275); //top point
+  vertex(130,311); //l
+  vertex(165,315); // r
+  endShape();
+
+  beginShape(); //squ right
+  vertex(442,130); //TL
+  vertex(452,150); //BL
+  vertex(473,140); //BR
+  vertex(464,119); //TR
+  endShape(CLOSE);
+
+  ellipse(160, 80, 60,55); //up one
+  ellipse(370, 250, 90,90); //down
+  
+  beginShape();
+  vertex(30,196); //LT
+  vertex(35,221); //LB
+  vertex(155,195); //RB
+  vertex(150,170); //RT 150 170
+  endShape(CLOSE);
+  
+  
+  //rect(9, 200, 107, 20);
   //ellipse(250, 330, 20, 80);
   //ellipse(250, 300, 50, 50);
   
